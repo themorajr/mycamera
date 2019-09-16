6a4ce0f1a18e46643880d707b760f42a579523da
พี่ปลาวาฬ <42457039+themorajr@users.noreply.github.com>
2019-09-16 15:33:45 +0700

    awd
    awd

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CameraScreen from './src/CameraScreen';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <CameraScreen />
+        );
+    };
+};
